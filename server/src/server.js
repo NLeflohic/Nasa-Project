877c6afee2e316991602ed29a32934aa3b6d3ff4
Nicolas Le Flohic <leflohic.nicolas@gmail.com>
2022-06-26 17:56:46 +0200

    Adding mongodb test but failed

--- server/src/server.js
+++ server/src/server.js
@@ -2,25 +2,16 @@ const http = require ('http');
 const app = require('./app');
 
 const {loadPlanetsData} = require ('./models/planets.model');
+const {mongoConnect} = require ('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = "mongodb+srv://nasa-api2:dgZ7zFLizkIspaS6@cluster0.slajv.mongodb.net/nasadb?retryWrites=true&w=majority"
-
 const server = http.createServer(app);
-const mongoos = require ('mongoose');
-const { default: mongoose } = require('mongoose');
+const mongoose = require ('mongoose');
 
-mongoose.connection.on('open', () => {
-  console.log('Mongo DB connection ready !');
-})
-
-mongoose.connection.on('error', (error) => {
-  console.log (error);
-})
 
 async function startServer () {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen (PORT, () => {
---
+++ server/src/services/mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require ('mongoose');
+
+const MONGO_URL = "mongodb+srv://nasa-api2:dgZ7zFLizkIspaS6@cluster0.slajv.mongodb.net/nasadb?retryWrites=true&w=majority"
+
+mongoose.connection.on('open', () => {
+  console.log('Mongo DB connection ready !');
+})
+
+mongoose.connection.on('error', (error) => {
+  console.log (error);
+})
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect () {
+  await mongoose.disconnect();
+}
+
+module.exports = mongoConnect;
