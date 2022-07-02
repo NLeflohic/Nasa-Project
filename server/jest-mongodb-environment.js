f7f756e6dd6d275aa7e380f261f12199cdbdcec4
Nicolas Le Flohic <leflohic.nicolas@gmail.com>
2022-07-02 15:41:34 +0200

    Add --passWithNoTest in client package.json

---
+++ server/jest-mongodb-environment.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '4.0.3',
+      skipMD5: true,
+    },
+    instance: {},
+    autoStart: false,
+  },
+  // mongoURLEnvName: "mongodb+srv://nasa-api2:dgZ7zFLizkIspaS6@cluster0.slajv.mongodb.net/nasadb?retryWrites=true&w=majority",
+  mongoURLEnvName:
+    'mongodb+srv://nasapi:GmPuLyPHGk5WoPq1@cluster0.slajv.mongodb.net/nasadb?retryWrites=true&w=majority',
+};
